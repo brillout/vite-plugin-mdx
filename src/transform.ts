99ec7a474698f767cdd3a71edbe74751bc48d94b
Romuald Brillout <git@brillout.com>
2021-03-02 09:56:45 +0100

    feat: help user upon wrong usage

--- src/transform.ts
+++ src/transform.ts
@@ -6,6 +6,8 @@ import { join as pathJoin } from 'path'
 export { transform }
 export { stopService }
 
+const pluginName = '@brillout/vite-plugin-mdx'
+
 async function transform({
   code_mdx,
   mdxOptions,
@@ -15,45 +17,10 @@ async function transform({
   mdxOptions?: any
   ssr?: boolean
 }): Promise<string> {
-  const preactInstalled = isPreactInstalled()
-  const reactInstalled = isReactInstalled()
-  let code_final = ''
-  if (!preactInstalled && !reactInstalled) {
-    const errorMessage = `Neither Preact Or React are installed. 
-    You must have one of these libraries installed to use this plugin.`
-    console.error(errorMessage)
-  } else {
-    const code_jsx = await mdxToJsx(code_mdx, mdxOptions)
-    const code_es2019 = await jsxToES2019(code_jsx)
-    code_final = injectImports(
-      code_es2019,
-      ssr,
-      preactInstalled,
-      reactInstalled
-    )
-  }
+  const code_jsx = await mdxToJsx(code_mdx, mdxOptions)
+  const code_es2019 = await jsxToES2019(code_jsx)
+  const code_final = injectImports(code_es2019, ssr)
   return code_final
-}
-
-function isPreactInstalled(): boolean {
-  let preactInstalled
-  try {
-    require('preact')
-    preactInstalled = true
-  } catch (err) {
-    preactInstalled = false
-  }
-  return preactInstalled
-}
-function isReactInstalled(): boolean {
-  let reactInstalled
-  try {
-    require('react')
-    reactInstalled = true
-  } catch (err) {
-    reactInstalled = false
-  }
-  return reactInstalled
 }
 
 async function mdxToJsx(code_mdx: string, mdxOptions: any) {
@@ -62,13 +29,13 @@ async function mdxToJsx(code_mdx: string, mdxOptions: any) {
 }
 
 async function jsxToES2019(code_jsx: string) {
-  // We use esbuild ourselves instead of letting Vite doing the esbuild transform,
+  // We use `esbuild` ourselves instead of letting Vite doing the esbuild transform,
   // because there don't seem to be a way to change the esbuild options for specific
   // files only: https://vitejs.dev/config/#esbuild
 
   const esBuild = await ensureEsbuildService()
 
-  /* Uncomment to inspect TransformOptions
+  /* Uncomment this to inspect the type `TransformOptions`
   type TransformOptions = Pick<Parameters<typeof esBuild.transform>, 1>[1];
   let t: TransformOptions;
   t!.format
@@ -81,7 +48,7 @@ async function jsxToES2019(code_jsx: string) {
     target: 'es2019'
   })
 
-  // TODO stabelize this bugfix
+  // TODO stabilize this bugfix
   code_es2019 = code_es2019.replace(
     'export default function MDXContent',
     'export default MDXContent; function MDXContent'
@@ -90,48 +57,40 @@ async function jsxToES2019(code_jsx: string) {
   return code_es2019
 }
 
-function injectImports(
-  code_es2019: string,
-  ssr: boolean,
-  preactInstalled: boolean,
-  reactInstalled: boolean
-) {
-  let code_withImports = ''
-  if (preactInstalled) {
-    code_withImports = [
-      `import {h} from 'preact'`,
-      `import { mdx } from '${getMdxImportPath(ssr, 'preact')}'`,
-      '',
-      code_es2019
-    ].join('\n')
-  } else if (reactInstalled) {
-    code_withImports = [
-      `import React from 'react'`,
-      `import { mdx } from '${getMdxImportPath(ssr, 'react')}'`,
+function injectImports(code_es2019: string, ssr: boolean) {
+  if (isInstalled('preact')) {
+    return [
+      `import { h } from 'preact'`,
+      `import { mdx } from '${getMdxImportPath('@mdx-js/preact', ssr)}'`,
       '',
       code_es2019
     ].join('\n')
   }
 
-  return code_withImports
-}
-function getMdxImportPath(ssr: boolean, typeOfJSX: string): string {
-  let importToReturn = ''
-  switch (typeOfJSX) {
-    case 'react':
-      if (!ssr) {
-        importToReturn = '@mdx-js/react'
-      } else {
-        importToReturn = resolveEsmEntry('@mdx-js/react')
-      }
-    case 'preact':
-      if (!ssr) {
-        importToReturn = '@mdx-js/preact'
-      } else {
-        importToReturn = resolveEsmEntry('@mdx-js/preact')
-      }
+  if (isInstalled('react')) {
+    return [
+      `import React from 'react'`,
+      `import { mdx } from '${getMdxImportPath('@mdx-js/react', ssr)}'`,
+      '',
+      code_es2019
+    ].join('\n')
   }
-  return importToReturn
+
+  throw new Error(
+    `[Wrong Usage][${pluginName}] You need to \`npm install react\` or \`npm install preact\`.`
+  )
+}
+function getMdxImportPath(mdxPackageName: string, ssr: boolean): string {
+  if (!isInstalled(mdxPackageName)) {
+    throw new Error(
+      `[Wrong Usage][${pluginName}] You need to \`npm install ${mdxPackageName}\`.`
+    )
+  }
+  if (!ssr) {
+    return mdxPackageName
+  } else {
+    return resolveEsmEntry(mdxPackageName)
+  }
 }
 
 function resolveEsmEntry(moduleName: string): string {
@@ -139,6 +98,17 @@ function resolveEsmEntry(moduleName: string): string {
   const { module: esmPath } = packageJson
   const esmEntry = require.resolve(pathJoin(moduleName, esmPath))
   return esmEntry
+}
+
+function isInstalled(packageName: string): boolean {
+  let yes
+  try {
+    require(packageName)
+    yes = true
+  } catch (err) {
+    yes = false
+  }
+  return yes
 }
 
 let _service: Promise<Service> | undefined
