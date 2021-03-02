234c8d30b54015b8a595f5484d421cbad6c4cb3d
Romuald Brillout <git@brillout.com>
2021-03-02 10:05:48 +0100

    feat: let user decide whether to use remark-frontmatter

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { Plugin } from 'vite'
-import * as remarkFrontmatter from 'remark-frontmatter'
 import { stopService, transform } from './transform'
 
 export default createPlugin
 
 function createPlugin(mdxOptions?: any): Plugin {
-  mdxOptions = mdxOptions || {}
-  mdxOptions.remarkPlugins = mdxOptions.remarkPlugins || []
-  mdxOptions.remarkPlugins.unshift(remarkFrontmatter)
-
   return {
     name: 'vite-plugin-mdx',
     transform(code_mdx: string, id: string, ssr?: boolean) {
