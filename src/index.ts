58e4ce0938c59e66f90932acc0b07b620a490501
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-01 17:02:47 -0400

    feat: accept a function for file-specific options (#16)

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,30 @@ import { MdxOptions, MdxPlugin } from './types'
 
 export default createPlugin
 
-function createPlugin(mdxOptions: MdxOptions = {}): MdxPlugin {
-  mdxOptions.remarkPlugins ??= []
-  mdxOptions.rehypePlugins ??= []
+function createPlugin(
+  mdxOptions: MdxOptions | ((filename: string) => MdxOptions) = {}
+): MdxPlugin {
+  let getMdxOptions: ((filename: string) => MdxOptions) | undefined
+  let globalMdxOptions: any = mdxOptions
+  if (typeof mdxOptions === 'function') {
+    getMdxOptions = mdxOptions
+    globalMdxOptions = {}
+  }
+
+  // Ensure plugin arrays exist for other Vite plugins to manipulate.
+  globalMdxOptions.remarkPlugins ??= []
+  globalMdxOptions.rehypePlugins ??= []
 
   return {
     name: 'vite-plugin-mdx',
-    mdxOptions: mdxOptions as any,
+    mdxOptions: globalMdxOptions,
     configResolved(config) {
       const reactRefresh = config.plugins.find(
         (p) => p.name === 'react-refresh'
       )
       this.transform = async function (code, id, ssr) {
         if (/\.mdx?$/.test(id)) {
+          const mdxOptions = mergeOptions(globalMdxOptions, getMdxOptions?.(id))
           code = await transform(code, mdxOptions, config.root)
           const refreshResult = await reactRefresh?.transform!.call(
             this,
@@ -30,5 +41,24 @@ function createPlugin(mdxOptions: MdxOptions = {}): MdxPlugin {
     async closeBundle() {
       await stopService()
     }
+  }
+}
+
+function mergeOptions(globalOptions: MdxOptions, localOptions?: MdxOptions) {
+  if (!localOptions) {
+    return globalOptions
+  }
+  return {
+    ...globalOptions,
+    ...localOptions,
+    remarkPlugins: globalOptions.remarkPlugins!.concat(
+      localOptions.remarkPlugins || []
+    ),
+    rehypePlugins: globalOptions.rehypePlugins!.concat(
+      localOptions.rehypePlugins || []
+    ),
+    compilers: (globalOptions.compilers || []).concat(
+      localOptions.compilers || []
+    )
   }
 }
