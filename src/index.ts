88f780d05e3312f63c8c88ec7e11267ea198d46a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-05 18:16:10 -0400

    feat: prefer the local @mdx-js/mdx
    ..and cache any resolved imports.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+import { requireMdx } from './resolveImport'
 import { remarkMdxImport } from './remarkMdxImport'
 import { stopService, transform } from './transform'
 import { MdxOptions, MdxPlugin, RemarkPlugin } from './types'
-import mdx from '@mdx-js/mdx'
 import fs from 'fs'
 
 export default createPlugin
@@ -53,7 +53,7 @@ function createPlugin(
                   getMdxOptions?.(id).remarkPlugins
                 )
                 remarkPlugins.push(mdxImportPlugin)
-                return mdx.createMdxAstCompiler({
+                return requireMdx(root).createMdxAstCompiler({
                   remarkPlugins: remarkPlugins as any[]
                 })
               }
