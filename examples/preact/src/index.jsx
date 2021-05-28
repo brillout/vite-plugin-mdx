72c81647ef0be4ceee2c5f78e22f2403ea27ab78
icew4ll <icew4ll@tuta.io>
2021-05-28 16:52:00 +0000

    updated preact example and versions so example can compile

--- examples/preact/src/index.jsx
+++ examples/preact/src/index.jsx
@@ -1,5 +1,6 @@
 import { render } from 'preact'
 import './index.css'
-import Hello from './Hello.mdx'
+import Hello from './Hello.md'
+import "prismjs/themes/prism-tomorrow.css"
 
 render(<Hello />, document.getElementById('preact-root'))
--- examples/preact/vite.config.js
+++ examples/preact/vite.config.js
@@ -1,11 +1,23 @@
 import prefresh from '@prefresh/vite'
 import mdx from 'vite-plugin-mdx'
 
+// `options` are passed to `@mdx-js/mdx`
+const options = {
+  // See https://mdxjs.com/advanced/plugins
+  remarkPlugins: [
+    require('remark-prism')
+  ],
+  rehypePlugins: [],
+}
+
 export default {
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
     jsxInject: `import { h, Fragment } from 'preact'`
   },
-  plugins: [prefresh(), mdx()]
+  plugins: [
+    prefresh(),
+    mdx(options)
+  ]
 }
