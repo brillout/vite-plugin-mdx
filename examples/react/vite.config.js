e1e02a5ef14d7d1457bc1bc9c4c97e5930167001
Maciek Sakrejda <m.sakrejda@gmail.com>
2021-10-12 08:51:02 -0700

    Use import syntax instead of require syntax

--- examples/react/vite.config.js
+++ examples/react/vite.config.js
@@ -1,12 +1,15 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import mdx from 'vite-plugin-mdx'
 
+import remarkFrontmatter from 'remark-frontmatter';
+import { remarkMdxFrontmatter } from 'remark-mdx-frontmatter';
+
 // `options` are passed to `@mdx-js/mdx`
 const options = {
   // See https://mdxjs.com/advanced/plugins
   remarkPlugins: [
-    require('remark-frontmatter'),
-    require('remark-mdx-frontmatter').remarkMdxFrontmatter,
+    remarkFrontmatter,
+    remarkMdxFrontmatter,
   ],
   rehypePlugins: [],
 }
